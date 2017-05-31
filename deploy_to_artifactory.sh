020d7cc4cf90abc4f235e6027556376f4cb4e8ec
Tim Urmancheev <tim.urmancheev@gmail.com>
2017-05-31 19:23:41 +0300

    3.3.305 release and script to deploy maven artifact (305 is used because custom qualifier version is not supported. It’s large enough that if will never actually be used by upstream)

---
+++ deploy_to_artifactory.sh
@@ -0,0 +1,5 @@
+mvn deploy:deploy-file -Durl=http://artifactory.cg.ru/artifactory/libs-releases-local -DrepositoryId=central \
+  -Dfile=dist/bundles/biz/aQute/bnd/biz.aQute.bndlib/3.3.305/biz.aQute.bndlib-3.3.305-javadoc.jar \
+  -DpomFile=dist/bundles/biz/aQute/bnd/biz.aQute.bndlib/3.3.305/biz.aQute.bndlib-3.3.305.pom \
+  -Dsources=dist/bundles/biz/aQute/bnd/biz.aQute.bndlib/3.3.305/biz.aQute.bndlib-3.3.305-sources.jar \
+  -Djavadoc=dist/bundles/biz/aQute/bnd/biz.aQute.bndlib/3.3.305/biz.aQute.bndlib-3.3.305-javadoc.jar
